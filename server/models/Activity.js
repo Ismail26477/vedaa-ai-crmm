0c49570d000e4ab3b69ce72d6d6b3e457d0a0057
Ismail Sheikh <129026460+Ismail26477@users.noreply.github.com>
2025-12-27 11:42:34 +0530

    Update Activity.js

--- server/models/Activity.js
+++ server/models/Activity.js
@@ -5,7 +5,7 @@ const activitySchema = new mongoose.Schema(
     leadId: { type: mongoose.Schema.Types.ObjectId, ref: "Lead", required: true },
     type: {
       type: String,
-      enum: ["created", "assigned", "stage_changed", "call_logged", "note_added", "updated"],
+      enum: ["created", "assigned", "stage_changed", "call_logged", "note_added", "updated", "merged"],
       required: true,
     },
     description: { type: String, required: true },
